f0e2652b2307f1d0424b69592c11856b274d859d
tmerril7 <trav.merrill@gmail.com>
2023-07-08 13:25:48 -0600

    asdfas

--- ext/express-api/index.js
+++ ext/express-api/index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 const bodyParser = require("body-parser")
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+const { getFirestore, Timestamp, FieldValue, Filter, DocumentSnapshot } = require('firebase-admin/firestore');
 const { format, zonedTimeToUtc, utcToZonedTime } = require('date-fns-tz')
 
 const today = new Date()
